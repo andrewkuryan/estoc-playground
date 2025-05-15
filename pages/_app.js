bfbe2186d85f3108362a5a8a6fd9fa10784eed1d
andrewkuryan <andrewkuryan998@gmail.com>
2025-05-15 23:27:29 +0400

    Fix build failure due to global styles

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import './index.scss';
+
+export default function PlaygroundApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-import './page.scss';
+import styles from './page.module.scss';
 
 interface PageProps {
   code?: string;
@@ -14,9 +14,9 @@ export default function Page({ code }: PageProps) {
         <title>Estoc Playground</title>
         {code && <meta name="description" content={`Your code: ${code}`} />}
       </Head>
-      <form className="page-root" method="GET" action="/">
-        <textarea name="code" id="input" placeholder="Enter code here" defaultValue={code} />
-        <textarea id="result" placeholder="Output" readOnly={true} />
+      <form className={styles.pageRoot} method="GET" action="/">
+        <textarea name="code" id={styles.input} placeholder="Enter code here" defaultValue={code} />
+        <textarea id={styles.result} placeholder="Output" readOnly={true} />
         <button type="submit">Run</button>
       </form>
     </>
